4c52f788fefa45c6bd9bd3791a39e4105831b7d8
HUSMIZA FAZEHAH <94205762+FAZEHAHH@users.noreply.github.com>
2023-12-22 09:29:04 +0800

    Update DemandBean.java
    change to user

--- src/com/shashi/beans/DemandBean.java
+++ src/com/shashi/beans/DemandBean.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class DemandBean implements Serializable {
 
-	private String userName;
+	private String user;
 	private String prodId;
 	private int demandQty;
 
